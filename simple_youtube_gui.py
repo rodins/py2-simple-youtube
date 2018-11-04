72c968c3f74fc87c2fefe4d9e893595e882f9f9a
Sergey Rodin <rodin.s@rambler.ru>
2018-11-04 13:28:47 +0200

    Add toolbar with entry

--- simple_youtube_gui.py
+++ simple_youtube_gui.py
@@ -13,8 +13,29 @@ class Gui(gtk.Window):
         self.set_size_request(700, 400)
         #TODO: add app icon
 
-        
+        # Toolbar and it's items
+        toolbar = gtk.Toolbar()
+        toolbar.set_style(gtk.TOOLBAR_ICONS)
+
+        entry_item = gtk.ToolItem()
+        entry = gtk.Entry()
+        entry.set_tooltip_text("Search youtube")
+        entry.connect("activate", self.entry_activated)
+        entry_item.add(entry)
+        toolbar.insert(entry_item, -1)
+
+        vbox = gtk.VBox(False, 1)
+        vbox.pack_start(toolbar, False, False, 1)
+        toolbar.show_all()
+
+        self.add(vbox)
+        vbox.show()
         self.show()
+
+    def entry_activated(self, widget):
+        query = widget.get_text().strip()
+        if query != "":
+            print query
 
     def on_destroy(self, widget):
         gtk.main_quit()
